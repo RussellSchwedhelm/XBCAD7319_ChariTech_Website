919389fcfcca76417faec97271d39f0deedffd90
ST10065470 <st10065470@vcconnect.edu.za>
2024-09-13 09:17:20 +0200

    Add project files.

---
+++ XBCAD7319_ChariTech_Website.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace XBCAD7319_ChariTech_Website.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ XBCAD7319_ChariTech_Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XBCAD7319_ChariTech_Website.Global" Language="C#" %>
