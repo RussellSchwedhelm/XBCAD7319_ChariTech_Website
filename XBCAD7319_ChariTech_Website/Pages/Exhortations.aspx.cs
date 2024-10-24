b9ba368801d95df013f75377b74542b763456bdb
Isaac Nichols <st10058357@vcconnect.edu.za>
2024-10-24 12:37:38 +0200

    Commented list view

--- XBCAD7319_ChariTech_Website/Pages/Exhortations.aspx.cs
+++ XBCAD7319_ChariTech_Website/Pages/Exhortations.aspx.cs
@@ -39,8 +39,8 @@ namespace XBCAD7319_ChariTech_Website.Pages
              DataTable exhortations = exhortationManager.GetExhortationsByChurchID(churchID);
 
             // Clear any existing items in the ListView or DropDownList
-            // exhortationListView.DataSource = exhortations;
-            // exhortationListView.DataBind();
+             exhortationListView.DataSource = exhortations;
+             exhortationListView.DataBind();
 
             /*  // Create dummy data using a DataTable
               DataTable exhortations = new DataTable();
