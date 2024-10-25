da70a35b72090ed39e42e7b50e8be4ca85154f80
Brice Agnew <ST10072411@vcconnect.edu.za>
2024-10-25 15:24:25 +0200

    Fixed exhortation page

--- XBCAD7319_ChariTech_Website/Pages/Exhortations.aspx.designer.cs
+++ XBCAD7319_ChariTech_Website/Pages/Exhortations.aspx.designer.cs
@@ -60,15 +60,6 @@ namespace XBCAD7319_ChariTech_Website.Pages
         protected global::System.Web.UI.HtmlControls.HtmlAudio AudioPlayer;
 
         /// <summary>
-        /// TranscriptionLabel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label TranscriptionLabel;
-
-        /// <summary>
         /// SummaryLabel control.
         /// </summary>
         /// <remarks>
@@ -76,5 +67,14 @@ namespace XBCAD7319_ChariTech_Website.Pages
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label SummaryLabel;
+
+        /// <summary>
+        /// TranscriptionLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TranscriptionLabel;
     }
 }
